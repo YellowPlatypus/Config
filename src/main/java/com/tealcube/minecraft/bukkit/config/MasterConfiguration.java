fca8d7b2a5a410ed5dcd6d422c37028fd7cf1acf
Richard Harrah <topplethenunnery@gmail.com>
2015-06-16 22:28:22 -0400

    reverting to previous MasterConfiguration

--- src/main/java/com/tealcube/minecraft/bukkit/config/MasterConfiguration.java
+++ src/main/java/com/tealcube/minecraft/bukkit/config/MasterConfiguration.java
@@ -14,20 +14,20 @@
  */
 package com.tealcube.minecraft.bukkit.config;
 
-import org.bukkit.configuration.Configuration;
-import org.bukkit.configuration.MemoryConfiguration;
+import org.apache.commons.lang.math.NumberUtils;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
-public final class MasterConfiguration extends MemoryConfiguration {
+public final class MasterConfiguration {
+
+    private Map<String, Object> settingMap;
 
     public MasterConfiguration() {
-        super();
-    }
-
-    public MasterConfiguration(Configuration defaults) {
-        super(defaults);
+        settingMap = new ConcurrentHashMap<>();
     }
 
     public static MasterConfiguration loadFromFiles(SmartYamlConfiguration... yamlConfigurations) {
@@ -58,11 +58,49 @@ public final class MasterConfiguration extends MemoryConfiguration {
         }
     }
 
-    public List<String> getStringList(String key, List<String> fallback) {
-        if (!contains(key)) {
+    public void set(String key, Object object) {
+        if (settingMap == null) {
+            return;
+        }
+        settingMap.put(key, object);
+    }
+
+    public Map<String, Object> getSettingMap() {
+        return new HashMap<>(settingMap);
+    }
+
+    public boolean isSet(String key) {
+        return getSettingMap().containsKey(key);
+    }
+
+    public Object get(String key, Object fallback) {
+        return settingMap == null || !settingMap.containsKey(key) ? fallback : settingMap.get(key);
+    }
+
+    public String getString(String key) {
+        return getString(key, "");
+    }
+
+    public String getString(String key, String fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
             return fallback;
         }
-        Object val = get(key);
+        Object val = settingMap.get(key);
+        if (val instanceof String) {
+            return (String) val;
+        }
+        return String.valueOf(val);
+    }
+
+    public List<String> getStringList(String key) {
+        return getStringList(key, new ArrayList<String>());
+    }
+
+    public List<String> getStringList(String key, List<String> fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
+            return fallback;
+        }
+        Object val = settingMap.get(key);
         if (!(val instanceof List)) {
             return fallback;
         }
@@ -72,6 +110,78 @@ public final class MasterConfiguration extends MemoryConfiguration {
             ret.add(String.valueOf(o));
         }
         return ret;
+    }
+
+    public boolean getBoolean(String key) {
+        return getBoolean(key, false);
+    }
+
+    public boolean getBoolean(String key, boolean fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
+            return fallback;
+        }
+        Object val = settingMap.get(key);
+        if (val instanceof Boolean) {
+            return (Boolean) val;
+        }
+        if (val instanceof String) {
+            return Boolean.parseBoolean((String) val);
+        }
+        return fallback;
+    }
+
+    public int getInt(String key) {
+        return getInt(key, 0);
+    }
+
+    public int getInt(String key, int fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
+            return fallback;
+        }
+        Object val = settingMap.get(key);
+        if (val instanceof Integer) {
+            return (Integer) val;
+        }
+        if (val instanceof String) {
+            return NumberUtils.toInt((String) val, fallback);
+        }
+        return fallback;
+    }
+
+    public long getLong(String key) {
+        return getLong(key, 0);
+    }
+
+    public long getLong(String key, long fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
+            return fallback;
+        }
+        Object val = settingMap.get(key);
+        if (val instanceof Long) {
+            return (Long) val;
+        }
+        if (val instanceof String) {
+            return NumberUtils.toLong((String) val, fallback);
+        }
+        return fallback;
+    }
+
+    public double getDouble(String key) {
+        return getDouble(key, 0);
+    }
+
+    public double getDouble(String key, double fallback) {
+        if (settingMap == null || !settingMap.containsKey(key)) {
+            return fallback;
+        }
+        Object val = settingMap.get(key);
+        if (val instanceof Double) {
+            return (Double) val;
+        }
+        if (val instanceof String) {
+            return NumberUtils.toDouble((String) val, fallback);
+        }
+        return fallback;
     }
 
 }
